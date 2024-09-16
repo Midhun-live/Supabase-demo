cdccd1e7290faf1be496de310d9d39870f003a0e
Midhun <midhunchakkaravarthybaz@gmail.com>
2024-09-16 22:46:32 +0530

    change made for the .env file

--- src/config/supabaseConfig.js
+++ src/config/supabaseConfig.js
@@ -1,7 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+const REACT_APP_SUPABASE_URL="https://eqtflmxumuslydrtzlrz.supabase.co"
+const REACT_APP_SUPABASE_ANON_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVxdGZsbXh1bXVzbHlkcnR6bHJ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTk5MjEwMzMsImV4cCI6MjAzNTQ5NzAzM30.lctCWbvAULZaAAc13ODZ_kkAsXTt16ucA72bSjxsyns"
+
+const supabaseUrl = REACT_APP_SUPABASE_URL;
+const supabaseKey = REACT_APP_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
